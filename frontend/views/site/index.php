a133c53c5f96ae6e492f946c95ac952ff00ff9f1
warui-david <waruidavid57@gmail.com>
2020-09-23 09:48:23 +0300

    First commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+
+use yii\helpers\Url;
+
+/* @var $this yii\web\View */
+
+$this->title = 'David Portfolio';
+?>
+      <h1>David Warui</h1>
+      <h2>I'm a professional Python Developer from Nairobi Kenya</h2>
+      <a href="<?= Url::to(['site/about'])?>" class="btn-about">About Me</a>
